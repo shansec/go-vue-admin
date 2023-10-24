c7aaa7265655d0b92ea806178ff0ea5870896034
XYJ <1516462542@qq.com>
2023-10-24 18:19:31 +0800

    feat: 修改用户信息增加字段校验

--- model/system/sys_role.go
+++ model/system/sys_role.go
@@ -4,12 +4,12 @@ import "time"
 
 type SysRole struct {
 	RoleId    int        `json:"roleId" gorm:"primary_key;not null;unique;comment:角色ID;size:90"` // 角色编码
-	RoleName  string     `json:"roleName" gorm:"size:128;comment:角色名称;"`                         // 角色名称
+	RoleName  string     `json:"roleName" gorm:"size:128;comment:角色名称;"`                       // 角色名称
 	Status    string     `json:"status" gorm:"size:4;comment:状态;"`                               // 状态 1禁用 2正常
-	RoleKey   string     `json:"roleKey" gorm:"size:128;comment:角色代码;"`                          //角色代码
-	RoleSort  int        `json:"roleSort" gorm:"size:16;comment:角色排序;"`                          //角色排序
-	Flag      string     `json:"flag" gorm:"size:128;comment:角色标记;"`                             //
-	Remark    string     `json:"remark" gorm:"size:255;comment:角色备注;"`                           //备注
+	RoleKey   string     `json:"roleKey" gorm:"size:128;comment:角色代码;"`                        // 角色代码
+	RoleSort  int        `json:"roleSort" gorm:"size:16;comment:角色排序;"`                        // 角色排序
+	Flag      string     `json:"flag" gorm:"size:128;comment:角色标记;"`                           // 角色标记
+	Remark    string     `json:"remark" gorm:"size:255;comment:角色备注;"`                         // 备注
 	Admin     bool       `json:"admin" gorm:"size:4;"`
 	DataScope string     `json:"dataScope" gorm:"size:128;"`
 	CreatedAt time.Time  // 创建时间
