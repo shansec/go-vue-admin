cbae50c6050a5ef8472d38ad22556b35ff225204
shansec <1516462542@qq.com>
2024-05-17 18:40:09 +0800

    fix: api 分页 bug 修改

--- service/system/sys_api.go
+++ service/system/sys_api.go
@@ -6,6 +6,7 @@ import (
 	"github/shansec/go-vue-admin/global"
 	"github/shansec/go-vue-admin/model/system"
 	systemReq "github/shansec/go-vue-admin/model/system/request"
+
 	"gorm.io/gorm"
 )
 
@@ -81,11 +82,11 @@ func (apiService *ApiService) GetApisInfo(getApisInfo systemReq.GetApiList) (api
 	if getApisInfo.Method != "" {
 		db = db.Where("method = ?", getApisInfo.Method)
 	}
-	err = db.Limit(limit).Offset(offset).Find(&apis).Error
+	err = db.Count(&total).Error
 	if err != nil {
 		return nil, 0, errors.New("api 列表获取失败")
 	}
-	err = db.Count(&total).Error
+	err = db.Limit(limit).Offset(offset).Find(&apis).Error
 	if err != nil {
 		return nil, 0, errors.New("api 列表获取失败")
 	}
