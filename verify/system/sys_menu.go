e59d8e0621376024c50260e8148bee491de2dd4c
shansec <1516462542@qq.com>
2024-06-20 15:41:25 +0800

    feat: 菜单模块完善

--- verify/system/sys_menu.go
+++ verify/system/sys_menu.go
@@ -5,5 +5,5 @@ import "github/shansec/go-vue-admin/utils"
 var (
 	CreateMenuVerify = utils.Rules{"Path": {utils.NotEmpty()}, "Name": {utils.NotEmpty()}, "Component": {utils.NotEmpty()}, "Meta": {utils.NotEmpty()}}
 	DeleteMenuVerify = utils.Rules{"ID": {utils.NotEmpty()}}
-	UpdateMenuVerify = utils.Rules{"ID": {utils.NotEmpty()}, "Path": {utils.NotEmpty()}, "Name": {utils.NotEmpty()}, "Hidden": {utils.NotEmpty()}, "Component": {utils.NotEmpty()}, "Meta": {utils.NotEmpty()}}
+	UpdateMenuVerify = utils.Rules{"ID": {utils.NotEmpty()}, "Path": {utils.NotEmpty()}, "Name": {utils.NotEmpty()}, "Component": {utils.NotEmpty()}, "Meta": {utils.NotEmpty()}}
 )
